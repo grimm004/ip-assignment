9d4a98ce14e441f500ca252be7a3bb4bc28d8074
grimm004 <grimm004@gmail.com>
2020-01-24 07:48:20 +0000

    .

--- nlm.py
+++ nlm.py
@@ -7,8 +7,8 @@ import os
 # These parameter values are indicative. You should choose your own
 # according to properties of the method you want to demonstrate
 
-h = 5
-m = 7  # template_window_size
+h = 10
+m = 5  # template_window_size
 s = 21  # search_window_size
 
 ##############################################################
@@ -18,7 +18,7 @@ for image_file in glob.glob(".\\images\\*.png"):
 
     dst = cv2.fastNlMeansDenoisingColored(img, None, h, h, m, s)
 
-    path = ".\\processed_h%d_t%d_m%d" % (h, m, s)
+    path = ".\\processed_h%d_m%d_s%d" % (h, m, s)
     if not os.path.exists(path):
         os.mkdir(path)
     cv2.imwrite("%s\\%s" % (path, image_file.split("\\")[-1]), dst)
